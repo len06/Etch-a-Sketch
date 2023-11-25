e6c2a52c5bfc8aef603e9c09f4dc2b3e6e86c8d1
l3n06 <wylt06@gmail.com>
2023-11-25 16:23:58 +0100

    added some button functions and pen color

--- basic.js
+++ basic.js
@@ -1,3 +1,5 @@
+let color = 'black';
+
 function populateBoard(size){
     let board = document.querySelector('.board');
     let squares= board.querySelectorAll('div');
@@ -8,13 +10,15 @@ function populateBoard(size){
     let amount = size * size;
     for(let i = 0; i < amount; i++){
         let square = document.createElement('div');
-        square.style.backgroundColor = "blue";
+        square.addEventListener('mousemove', colorSquares);
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
     }
 }
 
 populateBoard(16);
 
+//function to change the size of the squares in the board.
 function changeSize(input){
     if( input >= 2 && input <= 100)
     {
@@ -22,6 +26,22 @@ function changeSize(input){
     }
     else
     {
-        alert("Invalid amount, reimput square size!");
+        alert("Invalid amount, reinput square size!");
     }
+}
+
+//function that colors all the squares.
+function colorSquares(){
+    if(color === 'random'){
+        this.style.backgroundColor  = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    }
+    else
+    {
+        this.style.backgroundColor = color;
+    }
+}
+
+//function to change the color of the pen
+function changeColor(changedColor){
+    color = changedColor;
 }
